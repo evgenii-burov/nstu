445a8bbe3ba65ee88b4f19c51f73746a724f18a8
borov <justacancer4head@gmail.com>
2025-02-13 11:27:17 +0700

    третья лаба пв

--- Numerical_computations/lab3/l3.mk
+++ Numerical_computations/lab3/l3.mk
@@ -1,3 +1,3 @@
-project : C:\PV\lab3\l3.exe .SYMBOLIC
+project : C:\Users\borov\Documents\PV\lab3\l3.exe .SYMBOLIC
 
-!include C:\PV\lab3\l3.mk1
+!include C:\Users\borov\Documents\PV\lab3\l3.mk1
--- Numerical_computations/lab3/l3src.for
+++ Numerical_computations/lab3/l3src.for
@@ -1,23 +1,42 @@
       program main
-      common/size/n
-        dimension mem(1000000)
+      common/size/n,n_al
+        real mem(10000)
         call readDimension()
-        call readVector(mem(1))
-        call printVector(mem(1))
+        call readVector(mem(1),n+1,'ia.txt')
+        n_al=int(mem(n+1))-1
+        call readVector(mem(n+2),n_al,'al.txt')
+        call readVector(mem(n+2+n_al),n_al,'ja.txt')
+        call readVector(mem(n+2+2*n_al),n,'di.txt')
         
+        call printVector(mem(1),n+1)
+        call printVector(mem(n+2),n_al)
+        call printVector(mem(n+2+n_al),n_al)
+        call printVector(mem(n+2+2*n_al),n)
         pause
       end
       
+      subroutine readVector(v, n_v, name)
+      dimension v(*)
+      character*6 name
+      open(1,file=name, status='old', err=1)
+      read(1,*)(v(i), i=1,n_v)
+      close(1)
+      return
+    1 print*,'Couldnt open ',name,' file'
+      pause 
+      stop
+      end
+      
       subroutine readDimension()
-        common/size/n
+        common/size/n,n_al
         n=-1
         open(1,file='dimension.txt', status='old', err=1)
         read(1,*)n
         close(1)
         if(n.lt.1)then
-            print*,'Incorrect parameter: n must be positive'
-            pause
-            stop
+          print*,'Incorrect parameter: n must be positive'
+          pause
+          stop
         endif
         return
         
@@ -26,52 +45,11 @@
         stop
       end
       
-      subroutine readMatrix(di, al, au, ja, ia)
-      common/size/n
-        dimension di(*), al(*), au(*), ja(*), ia(*)
-        
-        open(1,file='di.txt', status='old', err=1)
-        read(1,*)(di(i), i=1,n)
-        close(1)
-        
-        open(1,file='al.txt', status='old', err=1)
-        read(1,*)(v(i), i=1,n)
-        close(1)
-        
-        open(1,file='au.txt', status='old', err=1)
-        read(1,*)(v(i), i=1,n)
-        close(1)
-        
-        open(1,file='ja.txt', status='old', err=1)
-        read(1,*)(v(i), i=1,n)
-        close(1)
-        
-        open(1,file='ia.txt', status='old', err=1)
-        read(1,*)(v(i), i=1,n)
-        close(1)
-        
-        return
-    1   print*,'Couldnt open vector file'
-        pause 
-        stop      
-      end
-      
-      subroutine readVector(v)
-        common/size/n
+      subroutine printVector(v, n_v)
+        common/size/n,n_al
         dimension v(*)
-        open(1,file='vector.txt', status='old', err=1)
-        read(1,*)(v(i), i=1,n)
-        close(1)
-        return
-    1   print*,'Couldnt open vector file'
-        pause 
-        stop      
-      end
-      
-      subroutine printVector(v)
-        common/size/n
-        dimension v(*)
-        do i=1,n
-          print*,v(i)
+        do i=1,n_v
+          write(*,fmt='(f5.1,\)')v(i)
         enddo
+        print*,' '
       end
